06be63a6d0258be067f2ad8d92bfacde0737c84f
Irinaaaaaa <104566560+IrinaCv@users.noreply.github.com>
2022-04-28 11:19:25 +0000

    proba

--- main.cs
+++ main.cs
@@ -1,7 +1,11 @@
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+class Program
+{
+  public static void Main (string[] args)
+  {
+    
+    int a=5;
+    if(a == 5) Console.WriteLine ("Hello World");
   }
 }
