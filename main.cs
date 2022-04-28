b54ef920b3fb6bf55b1b5031a19dcbd1ba8eaac2
Irinaaaaaa <104566560+IrinaCv@users.noreply.github.com>
2022-04-28 11:22:53 +0000

    p

--- main.cs
+++ main.cs
@@ -4,8 +4,7 @@ class Program
 {
   public static void Main (string[] args)
   {
-    
-    int a=5;
-    if(a == 5) Console.WriteLine ("Hello World");
+    Console.WriteLine ("Hello World");
+    asdfghjuk
   }
 }
